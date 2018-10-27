6743aaffbd045c2025a320eb77ddce33d68f6763
Sami Koivu <sami.koivu@gmail.com>
2018-10-27 14:46:30 -0300

    Update src/test/java/net/sf/rej/util/ByteSerializerTest.java
    Test was 90% not implemented, implementation added

--- src/test/java/net/sf/rej/util/ByteSerializerTest.java
+++ src/test/java/net/sf/rej/util/ByteSerializerTest.java
@@ -17,6 +17,7 @@
 package net.sf.rej.util;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import org.junit.Assert;
 
@@ -34,7 +35,18 @@ public class ByteSerializerTest {
 	 */
 	@Test
 	public void testByteSerializer() {
-		Assert.fail("Not yet implemented");
+		{
+			ByteSerializer bs = new ByteSerializer(true);
+			bs.addLong(0x1122334455667788L);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, (byte)0x88}, bytes);
+		}
+		{
+			ByteSerializer bs = new ByteSerializer(false);
+			bs.addLong(0x1122334455667788L);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {(byte)0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11}, bytes);
+		}
 	}
 
 	/**
@@ -42,15 +54,30 @@ public class ByteSerializerTest {
 	 */
 	@Test
 	public void testGetBytes() {
-		Assert.fail("Not yet implemented");
+		// generate random test data
+		byte[] data = new byte[128];
+		Random r = new Random();
+		r.nextBytes(data); // fill array with random data
+
+		
+		ByteSerializer bs = new ByteSerializer(true);
+		bs.addBytes(data);
+		byte[] bytes = bs.getBytes();
+		Assert.assertArrayEquals(data, bytes);
 	}
 
 	/**
 	 * Test method for {@link net.sf.rej.util.ByteSerializer#addBytes(byte[])}.
 	 */
 	@Test
-	public void testAddBytesByteArray() {
-		Assert.fail("Not yet implemented");
+	public void testAddBytesByteArray() {		
+		ByteSerializer bs = new ByteSerializer(true);
+		bs.addBytes("hello".getBytes());
+		byte[] bytes = bs.getBytes();
+		Assert.assertArrayEquals(new byte[] {'h', 'e', 'l', 'l', 'o'}, bytes);
+		bs.addBytes(" world".getBytes());
+		bytes = bs.getBytes();
+		Assert.assertArrayEquals(new byte[] {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'}, bytes);
 	}
 
 	/**
@@ -58,7 +85,13 @@ public class ByteSerializerTest {
 	 */
 	@Test
 	public void testAddBytesByteArrayInt() {
-		Assert.fail("Not yet implemented");
+		ByteSerializer bs = new ByteSerializer(true);
+		bs.addBytes("hello".getBytes(), 3);
+		byte[] bytes = bs.getBytes();
+		Assert.assertArrayEquals(new byte[] {'h', 'e', 'l'}, bytes);
+		bs.addBytes(" world".getBytes(), 2);
+		bytes = bs.getBytes();
+		Assert.assertArrayEquals(new byte[] {'h', 'e', 'l', ' ', 'w'}, bytes);
 	}
 
 	/**
@@ -66,7 +99,13 @@ public class ByteSerializerTest {
 	 */
 	@Test
 	public void testAddBytesByteArrayIntInt() {
-		Assert.fail("Not yet implemented");
+		ByteSerializer bs = new ByteSerializer(true);
+		bs.addBytes("hello".getBytes(), 2, 2);
+		byte[] bytes = bs.getBytes();
+		Assert.assertArrayEquals(new byte[] {'l', 'l'}, bytes);
+		bs.addBytes(" world".getBytes(), 1, 3);
+		bytes = bs.getBytes();
+		Assert.assertArrayEquals(new byte[] {'l', 'l', 'w', 'o', 'r'}, bytes);
 	}
 
 	/**
@@ -74,7 +113,26 @@ public class ByteSerializerTest {
 	 */
 	@Test
 	public void testAddByte() {
-		Assert.fail("Not yet implemented");
+		{
+			ByteSerializer bs = new ByteSerializer(true);
+			bs.addByte(0);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {0}, bytes);
+		}
+		{
+			ByteSerializer bs = new ByteSerializer(true);
+			bs.addByte(1);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {1}, bytes);
+		}
+		{
+			ByteSerializer bs = new ByteSerializer(true);
+			bs.addByte(0xFF);
+			bs.addByte(55);
+			bs.addByte(91);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {(byte)0xFF, 55, 91}, bytes);
+		}
 	}
 
 	/**
@@ -82,7 +140,42 @@ public class ByteSerializerTest {
 	 */
 	@Test
 	public void testAddShort() {
-		Assert.fail("Not yet implemented");
+		{
+			ByteSerializer bs = new ByteSerializer(true);
+			bs.addShort(0);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {0, 0}, bytes);
+		}
+		{
+			ByteSerializer bs = new ByteSerializer(true);
+			bs.addShort(1);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {0, 1}, bytes);
+		}
+		{
+			ByteSerializer bs = new ByteSerializer(false); // small endian
+			bs.addShort(1);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {1, 0}, bytes);
+		}
+		{
+			ByteSerializer bs = new ByteSerializer(true);
+			bs.addShort(-1);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {(byte)0xff, (byte)0xff}, bytes);
+		}
+		{
+			ByteSerializer bs = new ByteSerializer(true); // big endian
+			bs.addShort(0x1234);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {0x12, 0x34}, bytes);
+		}
+		{
+			ByteSerializer bs = new ByteSerializer(false); // small endian
+			bs.addShort(0x1234);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {0x34, 0x12}, bytes);
+		}
 	}
 
 	/**
@@ -90,7 +183,30 @@ public class ByteSerializerTest {
 	 */
 	@Test
 	public void testAddInt() {
-		Assert.fail("Not yet implemented");
+		{
+			ByteSerializer bs = new ByteSerializer(true);
+			bs.addInt(0);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {0, 0, 0, 0}, bytes);
+		}
+		{
+			ByteSerializer bs = new ByteSerializer(true);
+			bs.addInt(1);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {0, 0, 0, 1}, bytes);
+		}
+		{
+			ByteSerializer bs = new ByteSerializer(true);
+			bs.addInt(0xFF00FF);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {0, (byte) 0xFF, 0, (byte) 0xFF}, bytes);
+		}
+		{
+			ByteSerializer bs = new ByteSerializer(true);
+			bs.addInt(-1);
+			byte[] bytes = bs.getBytes();
+			Assert.assertArrayEquals(new byte[] {(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF}, bytes);
+		}
 	}
 
 	/**
@@ -98,7 +214,15 @@ public class ByteSerializerTest {
 	 */
 	@Test
 	public void testSize() {
-		Assert.fail("Not yet implemented");
+		ByteSerializer bs = new ByteSerializer(true);
+		byte[] bytes = bs.getBytes();
+		Assert.assertEquals("array size 0", 0, bytes.length);
+		bs.addBytes(new byte[91]);
+		bytes = bs.getBytes();
+		Assert.assertEquals("array size 91", 91, bytes.length);
+		bs.addByte(66);
+		bytes = bs.getBytes();
+		Assert.assertEquals("array size 92", 92, bytes.length);
 	}
 
 	/**
@@ -110,25 +234,25 @@ public class ByteSerializerTest {
 			ByteSerializer bs = new ByteSerializer(true);
 			bs.addLong(0L);
 			byte[] bytes = bs.getBytes();
-			Assert.assertTrue("long value 0", Arrays.equals(new byte[] {0, 0, 0, 0, 0, 0, 0, 0}, bytes));
+			Assert.assertArrayEquals(new byte[] {0, 0, 0, 0, 0, 0, 0, 0}, bytes);
 		}
 		{
 			ByteSerializer bs = new ByteSerializer(true);
 			bs.addLong(1);
 			byte[] bytes = bs.getBytes();
-			Assert.assertTrue("long value 1", Arrays.equals(new byte[] {0, 0, 0, 0, 0, 0, 0, 1}, bytes));
+			Assert.assertArrayEquals(new byte[] {0, 0, 0, 0, 0, 0, 0, 1}, bytes);
 		}
 		{
 			ByteSerializer bs = new ByteSerializer(true);
 			bs.addLong(0xFF00FFL);
 			byte[] bytes = bs.getBytes();
-			Assert.assertTrue("long value FF00FF", Arrays.equals(new byte[] {0, 0, 0, 0, 0, (byte) 0xFF, 0, (byte) 0xFF}, bytes));
+			Assert.assertArrayEquals(new byte[] {0, 0, 0, 0, 0, (byte) 0xFF, 0, (byte) 0xFF}, bytes);
 		}
 		{
 			ByteSerializer bs = new ByteSerializer(true);
 			bs.addLong(-1L);
 			byte[] bytes = bs.getBytes();
-			Assert.assertTrue("long value -1", Arrays.equals(new byte[] {(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF}, bytes));
+			Assert.assertArrayEquals(new byte[] {(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF}, bytes);
 		}
 	}
 
